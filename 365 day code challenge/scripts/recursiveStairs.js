9ab33397804bc6f783e0dbeae385c6bb6513fbe8
scottcarpenter28 <scottcarpenter28@gmail.com>
2020-09-15 18:56:49 -0400

    Add day 96

---
+++ 365 day code challenge/scripts/recursiveStairs.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+  $("#output").click(function() {
+    var stairs = parseInt($("#stairs").val());
+    if (stairs > 0)
+      console.log(positveStairs(stairs - 1, stairs));
+    else if (stairs < 0)
+      console.log(negativeStairs(stairs + 1, Math.abs(stairs)));
+    else
+      console.log("No stairs");
+  });
+});
+
+function positveStairs(steps, length) {
+  if (steps == 0) {
+    var s = '';
+    for (var i = 0; i < length; i++)
+      s += '#';
+    return s;
+  }
+  else {
+    var s = '';
+    for (var i = 0; i < length; i++) {
+      if (i < steps)
+        s += '_';
+      else
+        s += '#';
+    }
+    return s + '\n' + positveStairs(steps - 1, length);
+  }
+}
+
+function negativeStairs(steps, length){
+  if (steps == 0) {
+    var s = '';
+    for (var i = 0; i < length; i++){
+        s+='#';
+    }
+    return s;
+  }
+  else {
+    var s = '';
+    for (var i = 0; i < length; i++) {
+      if (i < Math.abs(steps))
+        s += '_';
+      else
+        s += '#';
+    }
+    return s + '\n' + negativeStairs(steps + 1, length);
+  }
+}
