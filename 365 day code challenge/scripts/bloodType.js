3b52c570d114e09cbed104d102fab47445296a44
scottcarpenter28 <scottcarpenter28@gmail.com>
2020-12-04 09:28:53 -0500

    Add day 176

---
+++ 365 day code challenge/scripts/bloodType.js
@@ -0,0 +1,16 @@
+var cost;
+$(document).ready(function() {
+  // Get value on button click and show alert
+  $("#output").click(function() {
+      var rType = $('#rType').val();
+      var dType = $('#dType').val();
+
+      if(dType==rType ||
+         (dType=='O-') ||
+         (dType=='O+' && rType.includes('+')) ||
+         (rType=='AB+' && (rType.includes('A') || rType.includes('B') )))
+        $('#ans').text('True');
+      else
+        $('#ans').text('False');
+  });
+});
