66e5f847c17e2c4875e807050d8a6541d575ebb6
scottcarpenter28 <scottcarpenter28@gmail.com>
2020-12-24 08:17:34 -0500

    Add day 196

--- 365 day code challenge/scripts/completePattern2.js
+++ 365 day code challenge/scripts/completePattern2.js
@@ -2,8 +2,11 @@ $(document).ready(function() {
   // Get value on button click and show alert
   $("#output").click(function() {
       var num = parseInt($('#num').val());
-      for(var i=1; i<=num; i++)
-        console.log(i.toString().repeat(i));
-      console.log('')
+      for(var i=num; i>=1; i--){
+        var str='';
+        for(var k=num; k>=num-i+1; k--)
+          str+=k+'';
+        console.log(str);
+      }
   });
 });
