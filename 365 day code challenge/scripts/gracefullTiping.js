f7b450732b95551d83234619b854a007f14c1198
scottcarpenter28 <33328166+scottcarpenter28@users.noreply.github.com>
2021-01-30 09:33:53 -0500

    Add day 233

---
+++ 365 day code challenge/scripts/gracefullTiping.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+  $("#output").click(function() {
+    var num = parseInt($('#num').val());
+    if(num < 1)
+      $('#ans').text("Number must be greater than 0");
+    else
+      $('#ans').text(gracefullTip(num));
+  });
+});
+
+
+function gracefullTip(num){
+  num = getTip(num);
+  if(num <10)
+    return num;
+  else if(num < 100)
+    return roundTo(num, 5);
+  else if(num < 1000)
+    return roundTo(num, 50);
+  else
+    return roundTo(num, 500);
+}
+
+function getTip(num){
+  var tip = num*.15;
+  tip += num;
+  return Math.ceil(tip);
+}
+
+function roundTo(num, rnd){
+  for(var i = 0; i < rnd; i++){
+    var temp = num + i;
+    if(temp % rnd == 0)
+      return temp;
+  }
+  return num;
+}
