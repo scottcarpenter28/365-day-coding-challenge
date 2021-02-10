37af6590ed97f7e222854e6c6df72e6b719fde2f
scottcarpenter28 <scottcarpenter28@gmail.com>
2021-02-10 05:41:36 -0500

    Add day 244

---
+++ 365 day code challenge/scripts/cumulativeSum.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  // Get value on button click and show alert
+  $("#output").click(function() {
+    var numbers = $('#numbers').val().split(', ').map(n => parseInt(n))
+    for(var i = 1; i < numbers.length; i++)
+      numbers[i] = numbers[i-1] + numbers[i];
+    $('#ans').text(numbers.toString());
+  });
+});
