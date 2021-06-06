a4462884cf1880a07929d660021f3df50263211c
scottcarpenter28 <scottcarpenter28@gmail.com>
2021-06-06 10:42:38 -0400

    Add day 357

---
+++ 365 day code challenge/scripts/isEqual.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  // Get value on button click and show alert
+  $("#output").click(function() {
+    let arr1 = $("#arr1").val().split(', ');
+    let arr2 = $("#arr2").val().split(', ');
+    let option = $('#flag').val();
+    if(option == 'true')
+      option = true;
+    else
+      option = false;
+    $('#ans').text(arr1.isEqual(arr2, option));
+  });
+});
+
+
+Array.prototype.isEqual = function(arr, option){
+  for(var i = 0; i < this.length; i++){
+    if(arr[i]!=this[i] && !option)
+      return false;
+    else if(arr[i]==this[i] && !option)
+      continue
+    console.log(!option);
+    if(!arr.includes(this[i]) && option)
+      return false;
+  }
+  return true;
+}
